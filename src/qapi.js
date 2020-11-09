ec171b5bca7e9067944750ab9add98e87cfd602b
Ivan Filippov <sintel.inside@gmail.com>
2020-11-09 15:25:52 +0300

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fa93407b83a2f001f2d966a",
+				"5fa93407b83a2f001f2d966c"
+			]
+		},
+		"5fa93407b83a2f001f2d966a": {
+			"id": "5fa93407b83a2f001f2d966a",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fa93407b83a2f001f2d966c": {
+			"id": "5fa93407b83a2f001f2d966c",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
